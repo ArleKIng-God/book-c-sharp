c769b80b96768a2e942985e875239b2d341a562c
ArleKing <55055751+ArleKIng-God@users.noreply.github.com>
2019-09-22 22:29:44 +0300

    Add files via upload

---
+++ лаб2/ConsoleApp9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string array = Console.ReadLine();
+
+            foreach (var i in array)
+            {
+                Console.WriteLine($"{i} -> {i - 97}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
